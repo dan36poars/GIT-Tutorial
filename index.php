e9409d20803d23c90cde4f8ac34bbb0ae295ba67
dan36poars <eldestemido@gmail.com>
2017-02-24 15:08:44 -0300

    Add form in index Signed-off-by: dan36poars <eldestemido@gmail.com>

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
 	<title>-- Screen For git hub --</title>
 </head>
 <body>
-	<?php 
-	echo 'hello World';
-	 ?>
+<div class="main">
+<form name="PostForm" method="post" action="" enctype="multipart/formdata">
+	<label>
+		<span>Email:</span>
+		<input type="text" name="name"/>
+	</label>
+	<label>
+		<span>Senha:</span>
+		<input type="text" name="email"/>
+	</label>
+	<input type="submit" value="Logar" name="postForm"/>
+	<a href="" title="recovery" >Recuperar Senha</a>
+</form>
+</div>
 </body>
 </html>
